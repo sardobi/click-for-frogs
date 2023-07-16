4337c51eba956579305332d8a4f564a8602005e6
sardobi <sardobi@googlemail.com>
2023-07-16 19:05:28 +0100

    Enable 1-8 keys for frog playing

--- popup/frogge_play.js
+++ popup/frogge_play.js
@@ -1,15 +1,15 @@
 /**
+* Given the name of a frog, get the corresponding note audio file.
+*/
+function frogToAudioFile(frogName) {
+    noteName = frogName.replace("Frog", "");
+    return browser.runtime.getURL(`notes/${noteName}.mp3`);
+}
+
+/**
 * Listen for hovers on the buttons, playing the corresponding frog note.
 */
 document.addEventListener("mouseover", (e) => {
-    /**
-    * Given the name of a frog, get the corresponding note audio file.
-    */
-    function frogToAudioFile(frogName) {
-        noteName = frogName.replace("Frog", "");
-        return browser.runtime.getURL(`notes/${noteName}.mp3`);
-    }
-
     if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
         // Ignore when mouseover is not on a button within <div id="popup-content">.
         return;
@@ -19,3 +19,25 @@ document.addEventListener("mouseover", (e) => {
     frogAudio = new Audio(frogToAudioFile(frogName));
     frogAudio.play();
 });
+
+/**
+ * Listen for keypresses to allow the frogs to be played with 1-8
+ */
+document.addEventListener("keydown", (e) => {
+    function frogName() {
+        switch (e.key) {
+            case "1": return "FrogCLow";
+            case "2": return "FrogD";
+            case "3": return "FrogE";
+            case "4": return "FrogF";
+            case "5": return "FrogG";
+            case "6": return "FrogA";
+            case "7": return "FrogB";
+            case "8": return "FrogCHigh";
+        }
+    };
+
+    frogName = frogName();
+    frogAudio = new Audio(frogToAudioFile(frogName));
+    frogAudio.play();
+})
