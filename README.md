7fcba157ca99fb5c56c011fcf1347aa3a1f0a327
sardobi <sardobi@googlemail.com>
2023-07-16 12:44:47 +0100

    Initial commit with WIP extension

---
+++ README.md
@@ -0,0 +1,6 @@
+# Frogges
+
+Recreates Tumblr's [Click for frogs][frogs] feature as best I can as a Firefox
+extension.
+
+[frogs]: https://www.tumblr.com/prideplus/722085154728869888/hello-fine-funky-little-friends-sadly-we-had?source=share
---
+++ popup/frogge_play.js
@@ -0,0 +1,21 @@
+/**
+* Listen for hovers on the buttons, playing the corresponding frog note.
+*/
+document.addEventListener("mouseover", (e) => {
+    /**
+    * Given the name of a frog, get the corresponding note audio file.
+    */
+    function frogToAudioFile(frogName) {
+        noteName = frogName.replace("Frog", "");
+        return browser.runtime.getURL(`notes/${noteName}.mp3`);
+    }
+
+    if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+        // Ignore when mouseover is not on a button within <div id="popup-content">.
+        return;
+    }
+
+    frogName = e.target.textContent;
+    frogAudio = new Audio(frogToAudioFile(frogName));
+    frogAudio.play();
+});
