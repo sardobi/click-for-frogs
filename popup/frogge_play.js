21bb5f7f4c97881b89703c866e1b5db1429a745c
sardobi <sardobi@googlemail.com>
2024-05-30 00:58:22 +0100

    The frogs hop when played

--- popup/frogge_play.js
+++ popup/frogge_play.js
@@ -1,3 +1,33 @@
+frogSpritesPath = "../icons/buttons"
+
+frogSpritesGrounded = {
+    1: frogSpritesPath + "/frogge-green.png",
+    2: frogSpritesPath + "/frogge-red.png",
+    3: frogSpritesPath + "/frogge-pink.png",
+    4: frogSpritesPath + "/frogge-yellow.png",
+    5: frogSpritesPath + "/frogge-green.png",
+    6: frogSpritesPath + "/frogge-red.png",
+    7: frogSpritesPath + "/frogge-pink.png",
+    8: frogSpritesPath + "/frogge-yellow.png",
+}
+
+frogSpritesHopping = {
+    1: frogSpritesPath + "/frogge-green-hop.png",
+    2: frogSpritesPath + "/frogge-red-hop.png",
+    3: frogSpritesPath + "/frogge-pink-hop.png",
+    4: frogSpritesPath + "/frogge-yellow-hop.png",
+    5: frogSpritesPath + "/frogge-green-hop.png",
+    6: frogSpritesPath + "/frogge-red-hop.png",
+    7: frogSpritesPath + "/frogge-pink-hop.png",
+    8: frogSpritesPath + "/frogge-yellow-hop.png",
+}
+
+function frogRibbits(frogNumber) {
+    playFrogAudio(frogNumber);
+
+    frogHops(frogNumber);
+}
+
 function playFrogAudio(frogNumber) {
     frogAudio = new Audio(browser.runtime.getURL(`sounds/frogge.mp3`));
 
@@ -9,14 +39,27 @@ function playFrogAudio(frogNumber) {
     frogAudio.play();
 }
 
+function frogHops(frogNumber) {
+    // Get the button with the corresponding frog number
+    const button = document.getElementById(frogNumber);
+
+    // Change the background image to the hopping frog image
+    button.style.backgroundImage = 'url(' + frogSpritesHopping[frogNumber] + ')';
+
+    // Reset the image back to the original after a delay to simulate the frog "landing"
+    setTimeout(() => {
+        button.style.backgroundImage = 'url(' + frogSpritesGrounded[frogNumber] + ')';
+    }, 200);
+
+}
+
 // Style the buttons with the corresponding frog images.
 document.querySelectorAll('.frog-button').forEach(button => {
-    button.style.setProperty('--img-src', 'url(' + button.dataset.imgSrc + ')');
+    frogNumber = button.id;
+    button.style.backgroundImage = 'url(' + frogSpritesGrounded[frogNumber] + ')';
 });
 
-/**
-* Listen for hovers on the buttons, playing the corresponding frog note.
-*/
+// Listen for hovers on the buttons, playing the corresponding frog note.
 document.addEventListener("mouseover", (e) => {
     if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
         // Ignore when mouseover is not on a button within <div id="popup-content">.
@@ -24,12 +67,10 @@ document.addEventListener("mouseover", (e) => {
     }
 
     frogNumber = e.target.id;
-    playFrogAudio(frogNumber);
+    frogRibbits(frogNumber);
 });
 
-/**
- * Listen for keypresses to allow the frogs to be played with 1-8
- */
+// Listen for keypresses to allow the frogs to be played with 1-8.
 document.addEventListener("keydown", (e) => {
     // Only play for 1-8
     switch (e.key) {
@@ -47,5 +88,5 @@ document.addEventListener("keydown", (e) => {
     }
 
     frogNumber = e.key;
-    playFrogAudio(frogNumber);
+    frogRibbits(frogNumber);
 })
